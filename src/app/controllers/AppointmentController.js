094085146002078cb6664f5e4f6409173f4c6b19
pereirls <lucas_sp100@hotmail.com>
2020-07-09 23:23:34 -0300

    adiconando listagem, cancelamento e notificações de agendamentos

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,9 +1,46 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore, format, subHours } from 'date-fns';
+import pt from 'date-fns/locale/pt'
 
 import User from '../models/User';
+import File from '../models/File';
 import Appointment from '../models/Appointment';
+import Notification from '../schemas/Notification';
 
 class AppointmentController {
+
+  async index(req, res) {
+
+    const { page = 1 } = req.query;
+
+    const appointments = await Appointment.findAll({
+      where: {
+        user_id: req.userId,
+        canceled_at: null,
+      },
+      order: ['date'],
+      limit: 20,
+      offset: (page - 1 ) * 20,
+      attributes: ['id', 'date'],
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['id', 'name'],
+          include: [
+            {
+              model: File,
+              as: 'avatar',
+              attributes: ['id', 'path', 'url']
+            },
+          ],
+        },
+      ],
+    });
+
+    return res.json(appointments);
+  }
+
   async store(req, res) {
     const schema = Yup.object().shape({
       date: Yup.date().required(),
@@ -12,18 +49,38 @@ class AppointmentController {
 
     if (!(await schema.isValid(req.body))) {
       return res.status(400).json({ error: 'Validation fail' });
-    }
+    };
 
     const { provider_id, date } = req.body;
 
     const isProvider = await User.findOne({
       where: { id: provider_id, provider: true },
-    })
+    });
 
     if(!isProvider) {
       return res
       .status(401)
       .json({ error: 'You can only create appointments with providers' });
+    }
+
+    var hourStart = startOfHour(parseISO(date));
+
+    if(isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Past date are not permitted' });
+    }
+
+    const checkAvailability = await Appointment.findOne({ where: {
+      provider_id,
+      canceled_at: null,
+      date: hourStart,
+    } })
+
+    if(checkAvailability) {
+      return res.status(400).json({ error: 'Appointment date is not available' });
+    }
+
+    if(req.userId === provider_id) {
+      return res.status(400).json({ error: 'A user cannot schedule for himself' });
     }
 
     const appointment = await Appointment.create({
@@ -32,7 +89,50 @@ class AppointmentController {
       date,
     })
 
+    const user = await User.findByPk(req.userId);
+
+    const formmattedDate = format(
+      hourStart,
+      "'dia' dd 'de' MMMM', às' H:mm'h' ",
+      { locale: pt }
+    );
+
+    await Notification.create({
+      content: `Novo agendamento de ${user.name} para o ${formmattedDate}`,
+      user: provider_id,
+    });
+
     return res.json(appointment )
+  }
+
+  async delete(req, res) {
+    const appointment = await Appointment.findByPk(req.params.id);
+
+    if(appointment.user_id !== req.userId) {
+      return res.status(401).json({
+        error: "You don't have permission to cancel this appointment"
+      });
+    }
+
+    const dateWithSub = subHours(appointment.date, 2);
+
+    if(isBefore(dateWithSub, new Date())) {
+      return res.status(401).json({
+        error: "You can only cancel appointments 2 hours id advance."
+      });
+    }
+
+    if(appointment.canceled_at!== null) {
+      return res.status(400).json({
+        error: "This appointment is already canceled."
+      });
+    }
+
+    appointment.canceled_at = new Date();
+
+    await appointment.save();
+
+    return res.json(appointment);
   }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,8 @@ import MulterConfig from './config/multer';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
+import NotificationController from './app/controllers/NotificationController';
 
 const routes = new Router();
 const upload = multer(MulterConfig);
@@ -20,8 +22,15 @@ routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
 
+routes.get('/appointments', AppointmentController.index);
 routes.put('/appointments', AppointmentController.store);
+routes.delete('/appointments/:id', AppointmentController.delete);
 
-routes.post('/files', upload.single('file'), FileController.store)
+routes.get('/schedule', ScheduleController.index);
+
+routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
